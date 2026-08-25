d85d22f6dd8caf83dd444150f8fb5e6503778082
ADAMZUHRUFANANDAFARENDRA <adam.22055@mhs.unesa.ac.id>
2026-08-25 17:00:43 +0700

    perubahan infrastruktur

--- backend/app.py
+++ backend/app.py
@@ -109,6 +109,7 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=True)
     password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), default='operator')
 
